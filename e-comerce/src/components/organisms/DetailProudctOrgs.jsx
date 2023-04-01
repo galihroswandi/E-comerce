c93eb7835d529df0da234b0ad90334278f80eeec
Galih Roswandi <galihroswandi25@gmail.com>
2023-04-01 22:09:59 +0700

    Update halaman detail product

--- e-comerce/src/components/organisms/DetailProudctOrgs.jsx
+++ e-comerce/src/components/organisms/DetailProudctOrgs.jsx
@@ -7,17 +7,17 @@ import CartIcon from "./../../assets/icons/AddCart.svg";
 const DetailProductOrgs = () => {
   return (
     <div className="relative">
-      <div className="card mt-[5.7rem] mx-2 py-5 px-5 box-border bg-white rounded-xl shadow-lg shadow-slate-200 overflow-hidden mb-8">
-        <div className="mb-3 sm:flex sm:gap-5 sm:justify-between sm:items-center">
+      <div className="card mt-[5.7rem] mx-2 py-5 px-5 box-border bg-white rounded-xl shadow-lg shadow-slate-200 overflow-hidden mb-8 sm:container">
+        <div className="mb-3 sm:flex sm:gap-5 md:gap-0 sm:justify-between sm:items-center md:px-10 lg:px-20">
           <div className="img border-b mb-5 sm:border-none">
             <img
               src="http://127.0.0.1:4000/images/1680099816618-840501158-Product2.png"
               alt="Images"
-              className="w-full -mt-5 sm:w-[15rem]"
+              className="w-full -mt-5 sm:w-[20rem] lg:w-[30rem]"
             />
           </div>
           <div className="desc sm:mr-10">
-            <h1 className="text-xl text-slate-800 mb-8 font-extralight">
+            <h1 className="text-xl text-slate-800 mb-8 font-extralight lg:text-4xl">
               ASUS ZENBOOK C103
             </h1>
             <div className="counter w-56 grid grid-cols-2 items-center gap-0 mb-8">
@@ -52,17 +52,19 @@ const DetailProductOrgs = () => {
                 distinctio beatae saepe aperiam!
               </p>
             </div>
-            <div className="footer fixed bg-white bg-opacity-5 backdrop-blur-md bottom-0 z-10 left-0 right-0 py-4 px-7 border-t-2 flex justify-between items-center gap-3 sm:static sm:px-0 sm:gap-0 sm:justify-center">
+            <div className="footer fixed bg-white bg-opacity-5 backdrop-blur-md bottom-0 z-10 left-0 right-0 py-4 px-7 border-t-2 flex justify-between items-center gap-3 sm:static sm:px-0 sm:gap-0 sm:justify-center lg:gap-2">
               <Link className="bg-green-500 text-white py-2 px-3 rounded-lg text-md font-extralight text-center leading-5 flex justify-center item sm:rounded-none sm:py-[.4rem]">
                 <img
                   src={CartIcon}
                   alt="Cart Icons"
-                  className="text-white fill-white hidden sm:block"
+                  className="text-white fill-white hidden sm:block lg:hidden"
                 />
-                <p className="sm:hidden">Tambah Keranjang</p>
+                <p className="sm:hidden lg:inline-block lg:py-2 lg:px-2">
+                  Tambah Keranjang
+                </p>
               </Link>
               <Link className="text-center border border-green-500 py-2 px-4 rounded-lg text-md font-extralight text-green-500 leading-5 sm:rounded-none">
-                Beli Langsung
+                <p className="lg:py-[.35rem] lg:px-2">Beli Langsung</p>
               </Link>
             </div>
           </div>
