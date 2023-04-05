422f68051a49b8fc134a29552b0de613c87a7382
Galih Roswandi <galihroswandi25@gmail.com>
2023-04-05 12:30:48 +0700

    Percobaan2

--- e-comerce/src/components/organisms/Pages/Ringkasan.jsx
+++ e-comerce/src/components/organisms/Pages/Ringkasan.jsx
@@ -6,10 +6,10 @@ const Ringkasan = (props) => {
       <div className="ringkasan mx-2 py-5 px-5 pb-8 box-border bg-white bg-opacity-5 rounded-xl shadow-lg shadow-slate-200 overflow-hidden mb-8 sm:container border border-green-500">
         <h1 className="text-xl text-slate-800 mb-4">Ringkasan</h1>
         <div className="desc">
-          <p className="text-slate-400 flex justify-between text-base py-3 border-b mb-3">
+          <p className="text-slate-400 flex justify-between text-base py-3 border-b mb-4">
             Total Harga (Barang 1) <span>Rp30.000.000</span>
           </p>
-          <p className="text-slate-800 flex justify-between text-xl mb-8">
+          <p className="text-slate-800 flex justify-between text-lg mb-8">
             Total Bayar <span>Rp150.000.000</span>
           </p>
           <button className="bg-green-500 min-w-full py-2 px-1 text-base text-white rounded-sm">
