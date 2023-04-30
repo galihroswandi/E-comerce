5789cec97ecc261a90bcec56ff6ef1d2caafb5b8
unknown <dzikrirahmadani211@gmail,com>
2023-04-30 17:39:00 +0700

    login

---
+++ src/Pages/Login/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Navbar from "../../components/Elements/Navbar";
+import Banner from "../../components/Fragments/Banner";
+import Categories from "../../components/Fragments/Kategori";
+import AllProducts from "../../components/Fragments/AllProducts";
+import Footer from "../../components/Elements/Footer";
+
+const Login = () => {
+  return (
+    <div>
+        <Navbar />
+        <div className="">
+          <div className="flex-div">
+            <div className="kanan">
+              <img src="" alt="" />
+            </div>
+            <div className="kiri">
+              <form action="">
+                
+              </form>
+            </div>
+          </div>
+        </div>
+        <Footer />
+    </div>
+  );
+};
+
+export default Login;
