526923b9e8eb62410b00e250654c7aa4d3d60d3e
Galih Roswandi <galihroswandi25@gmail.com>
2023-05-01 17:15:50 +0700

    Update Ukuran 3

--- src/components/Elements/Navbar/index.jsx
+++ src/components/Elements/Navbar/index.jsx
@@ -87,7 +87,7 @@ const Navbar = (props) => {
               id="search"
               autoComplete="off"
               placeholder="Cari product..."
-              className="max-w-[78%] py-[.40rem] px-2 text-xs rounded-l-md border border-green-500 outline-none text-slate-700"
+              className="max-w-[78%] py-[.40rem] px-2 text-sm rounded-l-md border border-green-500 outline-none text-slate-700"
             />
             <button
               type="submit"
